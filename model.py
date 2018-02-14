49dddb88e7d11335bec90c4088e56b483065b2a3
bzhang70 <bzhang70@illinois.edu>
2018-02-14 00:54:13 -0600

    md and mps

---
+++ model.py
@@ -0,0 +1,55 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('../data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = '../data/IMG/' + filename
+        image = cv2.imread(current_path)
+        images.append(image)
+        measurement = float(line[3])
+        measurements.append(measurement)
+
+augment_images , augment_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augment_images.append(image)
+    augment_images.append(cv2.flip(image,1))
+    augment_measurements.append(measurement)
+    augment_measurements.append(measurement * -1.0)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+
+model = Sequential()
+model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+model.save('model.h5')
+
