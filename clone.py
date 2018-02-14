de7e0a02ed75a0a07fd74b01d8a9370202912137
bzhang70 <bzhang70@illinois.edu>
2018-02-13 23:50:59 -0600

    modi

--- clone.py
+++ clone.py
@@ -33,7 +33,6 @@ y_train = np.array(measurements)
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=(160, 320, 3)))
@@ -41,8 +40,8 @@ model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
-model.add(Convolution2D(64,3,3,subsample=(2,2),activation='relu'))
-model.add(Convolution2D(64,3,3,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
