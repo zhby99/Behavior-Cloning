aa97d419f702557b2c536deb4130d23992267e86
bzhang70 <bzhang70@illinois.edu>
2018-02-13 23:48:17 -0600

    modi

--- clone.py
+++ clone.py
@@ -11,36 +11,42 @@ with open('../data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = '../data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = '../data/IMG/' + filename
+        image = cv2.imread(current_path)
+        images.append(image)
+        measurement = float(line[3])
+        measurements.append(measurement)
+
+augment_images , augment_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augment_images.append(image)
+    augment_images.append(cv2.flip(image,1))
+    augment_measurements.append(measurement)
+    augment_measurements.append(measurement * -1.0)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Activation
-from keras.layers.convolutional import Conv2D, ZeroPadding2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Conv2D(96,11,11,subsample=(4,4),border_mode='valid',activation='relu'))
-model.add(MaxPooling2D(pool_size=(3,3),strides=(2,2)))
-model.add(Conv2D(256,5,5,subsample=(1,1),border_mode='same',activation='relu'))
-model.add(Conv2D(384,3,3,subsample=(1,1),border_mode='same',activation='relu'))
-model.add(Conv2D(384,3,3,subsample=(1,1),border_mode='same',activation='relu'))
-model.add(Conv2D(256,3,3,subsample=(1,1),border_mode='same',activation='relu'))
-model.add(MaxPooling2D(pool_size=(3,3),strides=(2,2)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(64,3,3,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(64,3,3,subsample=(2,2),activation='relu'))
 model.add(Flatten())
-model.add(Dense(4096,activation='relu'))
-model.add(Dense(1000,activation='relu'))
-model.add(Dense(128,activation='relu'))
-model.add(Dense(16,activation='relu'))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
